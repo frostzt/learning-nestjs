e1a2c3ab816f084abc6008997657010b700bc28e
frostzt <aidenfrostbite@gmail.com>
2021-04-19 10:14:04 +0530

    Created users controller and services, wip

---
+++ src/users/users.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers(): any {
+    return [{ id: 0 }];
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return {
+      id: parseInt(id),
+    };
+  }
+}
